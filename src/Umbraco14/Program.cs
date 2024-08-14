dd11d462d7ee82a45a6a6b70117482294e990e1c
Alan McBee <com.github@codecharm.com>
2024-08-14 16:59:47 -0700

    Adding Umbraco14

--- src/Umbraco14/Program.cs
+++ src/Umbraco14/Program.cs
@@ -5,25 +5,12 @@ builder.CreateUmbracoBuilder()
     .AddWebsite()
     .AddDeliveryApi()
     .AddComposers()
-    //.ConfigureAppConfiguration((context, config) =>
-    //{
-    //    var settings = config.Build();
-    //    var keyVaultEndpoint = settings["AzureKeyVaultEndpoint"];
-    //    if (!String.IsNullOrEmpty(keyVaultEndpoint) && Uri.TryCreate(keyVaultEndpoint, UriKind.Absolute, out var validUri))
-    //    {
-    //        config.AddAzureKeyVault(validUri, new DefaultAzureCredential());
-    //    }
-    //})
     .Build();
 
 WebApplication app = builder.Build();
 
-//if (env.IsDevelopment())
-//{
-//    app.UseDeveloperExceptionPage();
-//}
-
 await app.BootUmbracoAsync();
+
 
 app.UseUmbraco()
     .WithMiddleware(u =>
