d5b35aefabb45052b63a5f803c97cde216670786
Alan McBee <com.github@codecharm.com>
2024-09-16 00:41:40 -0700

    Config diagnostics

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -16,12 +16,22 @@ builder.CreateUmbracoBuilder()
     .Build();
 
 
+// Add logging providers
+// builder.Logging.ClearProviders(); // Optionally clear existing providers
+builder.Logging.AddConsole();     // Add logging to console
+builder.Logging.AddDebug();       // Add logging to debug window (good for development)
+#if NET9_0_OR_GREATER
+builder.Logging.AddAzureWebAppDiagnostics(); // Azure-specific logging provider (for diagnostics)
+#endif
+
 // Get all the configuration values for debugging and write them to the logger
 var configDebugView = builder.Configuration.GetDebugView();
-Console.WriteLine("Configuration values:");
-Console.WriteLine(configDebugView);
 
 WebApplication app = builder.Build();
+
+// Get the ILogger and IConfiguration instances
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("Configuration values: {configDebugView}", configDebugView);
 
 await app.BootUmbracoAsync();
 
